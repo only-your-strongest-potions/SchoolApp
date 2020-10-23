d551f959fd1717088d5bb5b17c00aea2f527e9eb
Gabriel <only_your_strongest_potions@protonmail.com>
2020-10-23 17:06:02 +0100

    Refactored code in the Business layer

--- SchoolBusiness/Business.cs
+++ SchoolBusiness/Business.cs
@@ -8,22 +8,19 @@ namespace SchoolBusiness
         public void CreateStudent(int id, string name, double gpa)
         {
             var data = new Data();
-            var student = new Student { Id = id, Name = name, Gpa = gpa };
-            data.AddStudent(student);
+            data.AddStudent(new Student { Id = id, Name = name, Gpa = gpa });
         }
 
         public void CreateInstructor(int id, string name)
         {
             var data = new Data();
-            var instructor = new Instructor { Id = id, Name = name };
-            data.AddInstructor(instructor);
+            data.AddInstructor(new Instructor { Id = id, Name = name });
         }
 
         public void CreateCourse(int id, string name, int creditHours)
         {
             var data = new Data();
-            var course = new Course { Id = id, Name = name, CreditHours = creditHours };
-            data.AddCourse(course);
+            data.AddCourse(new Course { Id = id, Name = name, CreditHours = creditHours });
         }
     }
 }
