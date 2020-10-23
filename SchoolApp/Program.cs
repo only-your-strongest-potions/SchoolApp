96b5b1ead46c72e21be309cf869dc916b12b2556
Gabriel <2monitors1developer@protonmail.com>
2020-10-23 11:25:45 +0100

    Initial commit.

--- SchoolApp/Program.cs
+++ SchoolApp/Program.cs
@@ -2,9 +2,9 @@
 
 namespace SchoolApp
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
